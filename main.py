f905e746f4a1f2cf07a4d03ca86725636d8a7d2f
JuanCa-16 <juancamilohg162003@gmail.com>
2025-04-02 21:10:11 -0500

    fix main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from App.dataBase.dataBase import engine, Base
 from App.routers.tareas import router as tareas_router  # Importa el router de tareas
-
+import uvicorn
 # Crear tablas en la base de datos
 def create_tables():
     Base.metadata.create_all(bind=engine)
@@ -16,5 +16,9 @@ app.include_router(tareas_router, prefix="/tareas", tags=["Tareas"])
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
 # uvicorn main:app --reload
